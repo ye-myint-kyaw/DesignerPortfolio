760498889f3dd2210c1dce99e25580172ca7ed1d
yemyintkyaw <yemyintkyaw241220@gmail.com>
2024-08-10 17:54:28 +0700

    doing KV animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,47 @@
+"use client"
+
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/header/Header";
+import TopCont from "@/components/TopCont/TopCont";
+import { useEffect, useRef } from "react";
 
 export default function Top(){
+  const mousePosition = useRef({x:0, y:0})
+  const element = useRef<HTMLDivElement | null>(null);
+  
+  useEffect(()=>{
+    const moveMouse = (e:MouseEvent) =>{
+      mousePosition.current = { x: e.clientX, y: e.clientY };
+      console.log(mousePosition);
+      
+      if (element.current) {
+        console.log(element.current.style.maskPosition);
+        element.current.style.maskPosition = `${e.clientX-75}px ${e.clientY-75}px`;
+        element.current.style.backgroundColor = "white"
+      }
+    }
+    window.addEventListener("mousemove", moveMouse);
+
+    return ()=>{
+      window.removeEventListener("mousemove", moveMouse);
+    }
+  },[])
+
   return <div className="top">
     <main className="top__inner">
       <div className="topKv">
         <div className="topKv__up">
           <Header/>
+          <TopCont mainText="mid-weight,<br>minimalist" subText="Graphic designer"/>
         </div>
-        <div className="topKv__under">
-      
+        <div ref={element} className="topKv__under" >
+          <Header theme="light"/>
+          <TopCont theme="light" mainText="mid-weight,<br>minimalist" subText="Graphic designer"/>
+          {/* <TopCont theme="light" mainText="lowkey, <br>kind-hearted" subText="Visual artist"/> */}
         </div>
       </div>
     </main>
+    {/* <span id="circle"></span> */}
     <Footer/>
 
   </div>
--- src/components/TopCont/TopCont.tsx
+++ src/components/TopCont/TopCont.tsx
@@ -1,5 +1,19 @@
-export default function TopCont({theme="dark"}:{theme?: "dark"|"light"}){
-  return <div className="">
+type Props = {
+  subText:string,
+  mainText:string,
+  theme?: "dark"|"light"
+}
 
+const TopCont:React.FC<Props> = ({theme="dark", mainText, subText})=>{
+  return <div className={`topKvMain ${theme}`}>
+    <div className="topKvMain__inner">
+      <p className="topKvMain__greating">Hello, I am </p>
+      <p className="topKvMain__main" dangerouslySetInnerHTML={{
+        __html: mainText
+      }}/>
+      <p className="topKvMain__sub">{subText}</p>
+    </div>
   </div>
 }
+
+export default TopCont;
