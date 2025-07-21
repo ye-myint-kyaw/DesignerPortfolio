e601191296b8029850c7b0a1161262264fde127d
Ye Myint Kyaw <yemyintkyaw241220@gmail.com>
2025-07-21 23:31:28 +0700

    add animation to kv

--- src/components/Pages/Top/Kv.tsx
+++ src/components/Pages/Top/Kv.tsx
@@ -1,18 +1,39 @@
 "use client"
 import Link from 'next/link'
 import ROUTES from '@/constants/routes'
-import { Button, buttonVariants } from '@/components/ui/button'
+import { buttonVariants } from '@/components/ui/button'
 import BasicLayout from '@/components/Layout/BasicLayout'
+import ImageTrail from '@/components/ReactBit/ImageTrail/ImageTrail'
 
 const Kv = () => {
   return (
     <BasicLayout asChild>
-      <div className='min-h-screen flex flex-col justify-center items-start'>
+      <div className='min-h-screen flex flex-col justify-center items-start relative'>
         <p className='text-[20px] mb-2'>Hello👋</p>
         <p className='lg:text-[80px] md:text-[45px] text-[35px] lg:leading-[80px] md:leading-[45px] leading-[35px] font-bold mb-8'>I MAKE HIGH QUALITY <br className='hidden sm:block'/> UI/UX AND GRAPHIC DESIGN</p>
-        <Link href={ROUTES.CONTACT} className={`${buttonVariants({ variant: 'default' })} uppercase`} >
-          <span className='w-2 h-2 rounded-full bg-green ' aria-hidden></span> Available Now
+        <Link href={ROUTES.CONTACT} className={`${buttonVariants({ variant: 'default' })} uppercase relative z-50 overflow-hidden group`} >
+          <span className='w-2 h-2 rounded-full bg-green ' aria-hidden></span> 
+          <div className='flex flex-col max-h-full overflow-hidden'>
+            <span className='h-full min-h-[18.75px] transition-all ease-in-out duration-500 group-hover:-translate-y-full'>Available Now</span>
+            <span className='h-full min-h-[18.75px] text-center transition-all ease-in-out duration-500 group-hover:-translate-y-full'>Contact</span>
+          </div>
         </Link>
+        <div className='w-full h-full absolute overflow-hidden top-0 left-0 z-10'>
+          <ImageTrail
+            items={[
+              'https://picsum.photos/id/287/300/300',
+              'https://picsum.photos/id/1001/300/300',
+              'https://picsum.photos/id/1025/300/300',
+              'https://picsum.photos/id/1026/300/300',
+              'https://picsum.photos/id/1027/300/300',
+              'https://picsum.photos/id/1028/300/300',
+              'https://picsum.photos/id/1029/300/300',
+              'https://picsum.photos/id/1030/300/300',
+              'https://picsum.photos/id/287/300/300'
+            ]}
+            variant={8}
+          />
+        </div>
       </div>
     </BasicLayout>
   )
